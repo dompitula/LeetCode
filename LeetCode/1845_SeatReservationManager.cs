c0c2b005b55dbd83a082d3ea3a4184e1cadbc26a
dom <dom@pitula.pl>
2023-11-06 23:33:22 +0100

    added Solution to LeetCode problem #1845 - Seat Reservation Manager

---
+++ LeetCode/1845_SeatReservationManager.cs
@@ -0,0 +1,63 @@
+﻿//
+// Solution to LeetCode problem #1845 - Seat Reservation Manager
+//
+
+using LeetCode;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// Responsible for running the code.
+    /// </summary>
+    public class SeatManager
+    {
+
+        private int last;
+        private SortedSet<int> seats;
+
+        public SeatManager(int n)
+        {
+            last = 0;
+            seats = new SortedSet<int>();
+        }
+
+        public int Reserve()
+        {
+            if (!seats.Any())
+            {
+                return ++last;
+            }
+            else
+            {
+                int lowestSeat = seats.Min;
+                seats.Remove(lowestSeat);
+                return lowestSeat;
+            }
+        }
+
+        public void Unreserve(int seatNumber)
+        {
+            if (seatNumber == last)
+            {
+                --last;
+            }
+            else
+            {
+                seats.Add(seatNumber);
+            }
+        }
+    }
+}
+
+/// Paste in Main method to run.
+/*
+    SeatManager seatManager = new SeatManager(5);
+    Console.WriteLine(seatManager.Reserve());  // 1
+    Console.WriteLine(seatManager.Reserve());  // 2
+    seatManager.Unreserve(2);
+    Console.WriteLine(seatManager.Reserve());  // 2
+    Console.WriteLine(seatManager.Reserve());  // 3
+    Console.WriteLine(seatManager.Reserve());  // 4
+    Console.WriteLine(seatManager.Reserve());  // 5
+    seatManager.Unreserve(5);
+*/
